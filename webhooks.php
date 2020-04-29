6d66a4b4d1d92a64289400e80875175f2e1f46ee
raulmtz-1 <raulmartinezdelrio@hotmail.com>
2020-04-29 04:01:45 -0500

    sdk mercado pago

--- webhooks.php
+++ webhooks.php
@@ -1,4 +1,25 @@
-<?php
+ <?php
+
 // SDK de Mercado Pago
-	echo "test";
+require __DIR__ .  '/vendor/autoload.php';
+
+    MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+
+    switch($_POST["type"]) {
+        case "payment":
+            $plan = MercadoPago\Payment.find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
+    print_r($plan);
+    echo "tipo".$_POST["type"];
+
 ?>
