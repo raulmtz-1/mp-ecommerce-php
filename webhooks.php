891ff173334fe02e35f25270799097324f86624d
raulmtz-1 <raulmartinezdelrio@hotmail.com>
2020-04-30 02:27:38 -0500

    pull

--- webhooks.php
+++ webhooks.php
@@ -5,21 +5,21 @@ require __DIR__ .  '/vendor/autoload.php';
 
     MercadoPago\SDK::setAccessToken("APP_USR-6588866596068053-041607-428a530760073a99a1f2d19b0812a5b6-491494389");
 
-    switch($_POST["type"]) {
+    switch($_GET["type"]) {
         case "payment":
-            $plan = MercadoPago\Payment.find_by_id($_POST["id"]);
+            $plan = MercadoPago\Payment.find_by_id($_GET["id"]);
             break;
         case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            $plan = MercadoPago\Plan.find_by_id($_GET["id"]);
             break;
         case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            $plan = MercadoPago\Subscription.find_by_id($_GET["id"]);
             break;
         case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            $plan = MercadoPago\Invoice.find_by_id($_GET["id"]);
             break;
     }
     print_r($plan);
-    echo "tipo".$_POST["id"];
+    echo "id ".$_GET["id"];
 
 ?>
