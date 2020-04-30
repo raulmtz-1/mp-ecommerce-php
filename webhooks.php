2ee5cb8a78e2b02dcd36f358e5d8ddcbb5ff748e
raulmtz-1 <raulmartinezdelrio@hotmail.com>
2020-04-30 02:38:16 -0500

    pull

--- webhooks.php
+++ webhooks.php
@@ -1,25 +1,37 @@
- <?php
+<?php
+    MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
 
-// SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
+    $merchant_order = null;
 
-    MercadoPago\SDK::setAccessToken("APP_USR-6588866596068053-041607-428a530760073a99a1f2d19b0812a5b6-491494389");
-
-    switch($_GET["type"]) {
+    switch($_GET["topic"]) {
         case "payment":
-            $plan = MercadoPago\Payment.find_by_id($_GET["id"]);
+            $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+            // Get the payment and the corresponding merchant_order reported by the IPN.
+            $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
             break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_GET["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_GET["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_GET["id"]);
+        case "merchant_order":
+            $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
             break;
     }
-    //print_r($plan);
-    echo "id ".$_GET["id"];
+
+    $paid_amount = 0;
+    foreach ($merchant_order->payments as $payment) {
+        if ($payment['status'] == 'approved'){
+            $paid_amount += $payment['transaction_amount'];
+        }
+    }
+
+    // If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
+    if($paid_amount >= $merchant_order->total_amount){
+        if (count($merchant_order->shipments)>0) { // The merchant_order has shipments
+            if($merchant_order->shipments[0]->status == "ready_to_ship") {
+                print_r("Totally paid. Print the label and release your item.");
+            }
+        } else { // The merchant_order don't has any shipments
+            print_r("Totally paid. Release your item.");
+        }
+    } else {
+        print_r("Not paid yet. Do not release your item.");
+    }
 
 ?>
