4d7d2d013e894c1243995c3b56a4233031b0851a
raulmtz-1 <raulmartinezdelrio@hotmail.com>
2020-04-30 02:21:26 -0500

    pull

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 
-    MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+    MercadoPago\SDK::setAccessToken("APP_USR-6588866596068053-041607-428a530760073a99a1f2d19b0812a5b6-491494389");
 
     switch($_POST["type"]) {
         case "payment":
@@ -20,6 +20,6 @@ require __DIR__ .  '/vendor/autoload.php';
             break;
     }
     print_r($plan);
-    echo "tipo".$_POST["type"];
+    echo "tipo".$_POST["id"];
 
 ?>
